b9add093692e1bffc705b5d590e975008749a9ce
Domingo Suarez Torres <domingo.suarez@gmail.com>
2010-11-07 22:37:54 -0600

    updated config file to Grails 1.3.4

--- AvatarGrailsPlugin.groovy
+++ AvatarGrailsPlugin.groovy
@@ -13,8 +13,16 @@
  * limitations under the License.
  */
 class AvatarGrailsPlugin {
+	// the plugin version
     def version = 0.4
+	// the version or versions of Grails the plugin is designed for
+    def grailsVersion = "1.3.4 > *"
+	// the other plugins this plugin depends on
     def dependsOn = [:]
+	// resources that are excluded from plugin packaging
+    def pluginExcludes = [
+            "grails-app/views/error.gsp"
+    ]
 
     // TODO Fill in these fields
     def author = "Domingo Suarez Torres"
@@ -25,24 +33,24 @@ class AvatarGrailsPlugin {
 '''
 
     // URL to the plugin's documentation
-    def documentation = "http://grails.org/Avatar+Plugin"
+    def documentation = "http://grails.org/plugin/avatar"
+
+    def doWithWebDescriptor = { xml ->
+        // TODO Implement additions to web.xml (optional), this event occurs before 
+    }
 
     def doWithSpring = {
         // TODO Implement runtime spring config (optional)
     }
-   
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)		
-    }
 
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional)
-    }
-	                                      
     def doWithDynamicMethods = { ctx ->
         // TODO Implement registering dynamic methods to classes (optional)
     }
-	
+
+    def doWithApplicationContext = { applicationContext ->
+        // TODO Implement post initialization spring config (optional)
+    }
+
     def onChange = { event ->
         // TODO Implement code that is executed when any artefact that this plugin is
         // watching is modified and reloaded. The event contains: event.source,
