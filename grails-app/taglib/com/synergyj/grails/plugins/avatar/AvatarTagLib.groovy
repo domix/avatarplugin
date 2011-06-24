a2ad18eab7a791c30fb7382922046b847ad18295
bareich <g2011@brandyrox.com>
2011-06-24 19:16:01 +0200

    added extensive JavaDoc to gravatar-tag (refs GPAVATAR-1); shortened the code (mainly by introducing ternary operators for the attrs); restricted 'defaultGravatarUrl' to reasonable values; constrained 'size' to accept values according to the official Gravatar API definition

--- grails-app/taglib/com/synergyj/grails/plugins/avatar/AvatarTagLib.groovy
+++ grails-app/taglib/com/synergyj/grails/plugins/avatar/AvatarTagLib.groovy
@@ -19,104 +19,96 @@ import groovyx.net.http.HTTPBuilder
 import static groovyx.net.http.Method.GET
 
 class AvatarTagLib {
-    static namespace = "avatar"
+	static namespace = "avatar"
 
-    def gravatar = { attrs, body ->
-        if (!attrs.email) throw new IllegalStateException("Property [email] must be set!")
-        String email = attrs.email.toString()
-        def size = 20
-        def hash = MD5Util.md5Hex(email)
-        def alt = "Gravatar"
-        def cssClass = "avatar"
-        def title = ''
-        def id = ''
-        def name = ''
+	/** 
+	 * Outputs the Gravatar this is associated with the given email address
+	 * Note the parameter documentation at http://de.gravatar.com/site/implement/images/ 
+	 * 
+	 * @attr email		REQUIRED	the startDate for styling
+	 * @attr size					the disired dimensions in pixels for the gravatar image (from 1 up to 512)
+	 * @attr alt					alt-attribute for the resulting img-element
+	 * @attr cssClass				class-attribute for the resulting img-element
+	 * @attr title					title-attribute for the resulting img-element
+	 * @attr id						id-attribute for the resulting img-element
+	 * @attr name					name-attribute for the resulting img-element
+	 * @attr defaultGravatarUrl		the default image to display if no gravatar is found; may be a URL or one of the following (defaults to the official Gravatar logo):
+	 * 								<li>404: do not load any image if none is associated with the email hash, instead return an HTTP 404 (File Not Found) response
+	 * 								<li>mm: (mystery-man) a simple, cartoon-style silhouetted outline of a person (does not vary by email hash)
+	 * 								<li>identicon: a geometric pattern based on an email hash
+	 * 								<li>monsterid: a generated 'monster' with different colors, faces, etc
+	 * 								<li>wavatar: generated faces with differing features and backgrounds
+	 * 								<li>retro: awesome generated, 8-bit arcade-style pixelated faces
+	 * @attr gravatarRating			desired image rating censor-level; may be one of the following:
+	 * 								<li>g (default): suitable for display on all websites with any audience type.
+	 * 								<li>pg: may contain rude gestures, provocatively dressed individuals, the lesser swear words, or mild violence.
+	 * 								<li>r: may contain such things as harsh profanity, intense violence, nudity, or hard drug use.
+	 * 								<li>x: may contain hardcore sexual imagery or extremely disturbing violence.
+	 * @emptyTag
+	 */
+	def gravatar = { attrs, body ->
+		if (!attrs.email) throw new IllegalStateException("Property [email] must be set!")
+		String email		= attrs.remove('email').toString()
+		def hash			= MD5Util.md5Hex(email)
+		def size			= attrs.remove('size')		?: 20
+		def alt				= attrs.remove('alt')		?: "Gravatar"
+		def cssClass		= attrs.remove('cssClass')	?: "avatar"
+		def title			= attrs.remove('title')		?: ''
+		def id				= attrs.remove('id')		?: ''
+		def name			= attrs.remove('name')		?: ''
+		def dgu				= attrs.remove('defaultGravatarUrl')	?: grailsApplication.config.avatarPlugin.defaultGravatarUrl
+		def gravatarRating	= attrs.remove('gravatarRating')		?: grailsApplication.config.avatarPlugin.gravatarRating
 
-        def gravatarBaseUrl = "http://gravatar.com/avatar/"
-        if (request.isSecure()) {
-            gravatarBaseUrl = "https://secure.gravatar.com/avatar/"
-        }
+		def gravatarBaseUrl	= request.isSecure() ? "https://secure.gravatar.com/avatar/" : "http://gravatar.com/avatar/"
+		String gravatarUrl	= "$gravatarBaseUrl$hash"
+		gravatarUrl	+= dgu.matches(/404|mm|identicon|monsterid|wavatar|retro|http.*/) ? "?d=${dgu}" : ''
 
-        String gravatarUrl = "$gravatarBaseUrl$hash"
+		if (gravatarRating) {
+			gravatarUrl += (gravatarUrl.contains('?') ? '&' : '?') + "r=${gravatarRating}"
+		}
 
-        if (attrs.size) size = attrs.size
+		// The size is requested to gravatar in order to get the imaged in the requested size
+		// If we don't send the 's' parameter the image is received at 80x80
+		if (size >= 1 && size <= 512) {
+			gravatarUrl += "&s=${size}"
+		}
 
-        if (attrs.alt) alt = attrs.alt
+		out << """<img id="${id}" name="${name}" alt="$alt" class="$cssClass" height="$size" width="$size" src="$gravatarUrl" title="$title"/>"""
+	}
 
-        if (attrs.title) title = attrs.title
+	def twitter = { attrs, body ->
+		def user
+		try {
+			user = new XmlParser().parse("http://api.twitter.com/1/users/show.xml?screen_name=${attrs.user}")
+		} catch (FileNotFoundException) {
+			user = new XmlParser().parse("http://api.twitter.com/1/users/show.xml?screen_name=twitter")
+		}
 
-        if (attrs.cssClass) cssClass = attrs.cssClass
+		def image = user.profile_image_url.text()
 
-        if (attrs.name) name = attrs.name
+		def alt = "twitter"
+		def cssClass = "avatar"
+		def title = attrs.user
 
-        if (attrs.id) id = attrs.id
+		out << "<img alt='$alt' class='$cssClass' height='${attrs?.size ?: 20}' width='${attrs?.size ?: 20}' src='$image' title='$title'/>"
+	}
 
-        def dgu = null
-        if (attrs.defaultGravatarUrl) {
-            dgu = attrs.defaultGravatarUrl
-        } else {
-            dgu = grailsApplication.config.avatarPlugin.defaultGravatarUrl
-        }
-        if (dgu) {
-            gravatarUrl += "?d=${dgu}"
-        }
+	def facebook = { attrs, body ->
+		def url = "https://graph.facebook.com/${attrs?.user ?: 'facebook'}/picture"
+		new HTTPBuilder(url).request(GET) { req ->
+			response.success = { resp ->
+				def notFound = resp.headers.find { it.name.contains('WWW-Authenticate') }
+				if (notFound) {
+					url = "https://graph.facebook.com/facebook/picture"
+				}
+			}
+			response.failure = { resp ->
+			}
+		}
 
-        def gravatarRating = null
-        if (attrs.gravatarRating) {
-            gravatarRating = attrs.gravatarRating
-        } else {
-            gravatarRating = grailsApplication.config.avatarPlugin.gravatarRating
-        }
-        if (gravatarRating) {
-            if (gravatarUrl.contains('?')) {
-                gravatarUrl += "&r=${gravatarRating}"
-            }
-            else {
-                gravatarUrl += "?r=${gravatarRating}"
-            }
-        }
-
-        // The size is requested to gravatar in order to get the imaged in the requested size
-        // If we don't send the 's' parameter the image is received at 80x80
-        gravatarUrl += "&s=${size}"
-
-        out << "<img id='${id}' name='${name}' alt='$alt' class='$cssClass' height='$size' width='$size' src='$gravatarUrl' title='$title'/>"
-    }
-
-
-    def twitter = { attrs, body ->
-        def user
-        try {
-            user = new XmlParser().parse("http://api.twitter.com/1/users/show.xml?screen_name=${attrs.user}")
-        } catch (FileNotFoundException) {
-            user = new XmlParser().parse("http://api.twitter.com/1/users/show.xml?screen_name=twitter")
-        }
-
-        def image = user.profile_image_url.text()
-
-        def alt = "twitter"
-        def cssClass = "avatar"
-        def title = attrs.user
-
-        out << "<img alt='$alt' class='$cssClass' height='${attrs?.size ?: 20}' width='${attrs?.size ?: 20}' src='$image' title='$title'/>"
-    }
-
-    def facebook = { attrs, body ->
-        def url = "https://graph.facebook.com/${attrs?.user ?: 'facebook'}/picture"
-        new HTTPBuilder(url).request(GET) { req ->
-            response.success = { resp ->
-                def notFound = resp.headers.find { it.name.contains('WWW-Authenticate') }
-                if (notFound) {
-                    url = "https://graph.facebook.com/facebook/picture"
-                }
-            }
-            response.failure = { resp ->
-            }
-        }
-
-        def alt = "facebook"
-        def cssClass = "avatar"
-        def title = attrs.user
-        out << "<img alt='$alt' class='$cssClass' height='${attrs?.size ?: 20}' width='${attrs?.size ?: 20}' src='$url' title='$title'/>"
-    }
+		def alt = "facebook"
+		def cssClass = "avatar"
+		def title = attrs.user
+		out << "<img alt='$alt' class='$cssClass' height='${attrs?.size ?: 20}' width='${attrs?.size ?: 20}' src='$url' title='$title'/>"
+	}
 }
