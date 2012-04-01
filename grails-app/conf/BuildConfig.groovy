bb79de4ab1cd8654f60398f2cce28149020ed634
Domingo Suarez Torres <domingo.suarez@gmail.com>
2012-03-31 18:43:49 -0600

    fixed error in dependency

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,9 +3,9 @@ grails.project.dependency.resolution = {
   }
   log "warn"
   plugins{
-	test ":code-coverage:1.2.4" { export = false }
-	test ":spock:0.5-groovy-1.7" { export = false }
-	build ":release:1.0.2-SNAPSHOT" { export = false }
+	test (":code-coverage:1.2.4") { export = false }
+	test (":spock:0.5-groovy-1.7") { export = false }
+	build (":release:1.0.2-SNAPSHOT") { export = false }
   }
   repositories {
     mavenLocal()
