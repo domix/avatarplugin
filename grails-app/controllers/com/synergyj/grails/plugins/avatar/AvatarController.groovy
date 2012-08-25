64c63d773f7c6d27782259fe88309e2babaac99a
Domingo Suarez Torres <domingo.suarez@gmail.com>
2012-08-25 11:31:12 -0500

    formatted out some code

--- grails-app/controllers/com/synergyj/grails/plugins/avatar/AvatarController.groovy
+++ grails-app/controllers/com/synergyj/grails/plugins/avatar/AvatarController.groovy
@@ -16,8 +16,8 @@ package com.synergyj.grails.plugins.avatar
 
 class AvatarController {
 
-    def index = { }
-    def obtain = {
-        render(view: 'index', model: [email: params.email, twitter: params.twitter, facebook: params.facebook])
-    }
+  def index = { }
+  def obtain = {
+    render(view: 'index', model: [email: params.email, twitter: params.twitter, facebook: params.facebook])
+  }
 }
