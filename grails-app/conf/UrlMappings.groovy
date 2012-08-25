c65af8cc636304440d126409f653175e9e44236c
Domingo Suarez Torres <domingo.suarez@gmail.com>
2012-08-25 11:30:19 -0500

    added mapping to index

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,11 +1,11 @@
 class UrlMappings {
-    static mappings = {
-      "/$controller/$action?/$id?"{
-	      constraints {
-			 // apply constraints here
-		  }
-	  }
-      "/"(view:"/index")
-	  "500"(view:'/error')
-	}
+  static mappings = {
+    "/$controller/$action?/$id?" {
+      constraints {
+        // apply constraints here
+      }
+    }
+    "/"(controller: 'avatar', action: 'index')
+    "500"(view: '/error')
+  }
 }
